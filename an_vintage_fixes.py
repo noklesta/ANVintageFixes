535d78082f242c236a455c8980ead51287ca2b6b
Anders NÃ¸klestad <anders.noklestad@iln.uio.no>
2012-01-12 17:03:00 +0100

    Remove unused commands

--- an_vintage_fixes.py
+++ an_vintage_fixes.py
@@ -112,29 +112,3 @@ class AnPasteLeftCommand(PasteLines):
 class AnPasteRightCommand(PasteLines):
     def perform_standard_vintage_command(self):
         self.view.run_command('vi_paste_right')
-
-###########################################
-# Scrolling by more than one line a a time
-###########################################
-
-
-class ScrollingBase(sublime_plugin.TextCommand):
-    def scroll(self, direction, animate):
-        current_pos = self.view.viewport_position()
-        delta = self.view.line_height() * self.nlines
-        self.view.set_viewport_position((current_pos[0], current_pos[1] + delta * direction), animate)
-
-
-class AnScrollUpCommand(ScrollingBase):
-    def run(self, edit, nlines=3, animate=False):
-        self.nlines, self.animate = nlines, animate
-        self.scroll(-1, animate)
-
-
-class AnScrollDownCommand(ScrollingBase):
-    def run(self, edit, nlines=3, animate=False):
-        self.nlines, self.animate = nlines, animate
-        self.scroll(1, animate)
-
-#################
-#################
